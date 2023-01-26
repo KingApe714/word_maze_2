66a6a5cd4336d4be40cf6cb716fed375a6fa8f2e
Jonathan <jonathan.diaz714@gmail.com>
2023-01-26 03:54:40 -0500

    succssfully dragging and dropping random tiles

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     
     
     const gridCells = document.querySelectorAll('.grid-cell');
-    const randomTile = document.querySelector('.random-tile-outer-container');
+    const randomTile = document.querySelector('.random-tile');
     const tileSideBarContainer = document.querySelector('.tile-side-bar-container');
+
+    const mutationObserver = new MutationObserver(entries => {
+        if (entries[0].removedNodes.length) {
+            const newRandomTile = document.createElement('div');
+            newRandomTile.className = 'random-tile';
+            newRandomTile.draggable = 'true';
+            newRandomTile.innerHTML = '?';
+
+            newRandomTile.addEventListener('dragstart', () => {
+                newRandomTile.classList.add('dragging-random-cell');
+            })
+
+            newRandomTile.addEventListener('dragend', () => {
+                newRandomTile.classList.remove('dragging-random-cell');
+            })
+
+            tileSideBarContainer.appendChild(newRandomTile)
+        }
+    })
+
+    mutationObserver.observe(tileSideBarContainer, { childList: true })
 
     randomTile.addEventListener('dragstart', () => {
         randomTile.classList.add('dragging-random-cell');
@@ -11,43 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     randomTile.addEventListener('dragend', () => {
         randomTile.classList.remove('dragging-random-cell');
-
-        const newRandomTile = randomTile.cloneNode('deep');
-        newRandomTile.addEventListener('dragstart', () => {
-            newRandomTile.classList.add('dragging-random-cell');
-        });
-        newRandomTile.addEventListener('dragend', () => {
-            newRandomTile.classList.remove('dragging-random-cell');
-        })
-        tileSideBarContainer.appendChild(newRandomTile);
     });
 
     gridCells.forEach((gridCell, idx) => {
-        console.log(gridCell.children.length);
-        gridCell.addEventListener('dragstart', () => {
-            gridCell.classList.add('dragging-cell');
+        // console.log(gridCell.children.length);
+        // gridCell.addEventListener('dragstart', () => {
+        //     gridCell.classList.add('dragging-cell');
 
-            console.log(`dragging cell ${idx}`)
-        });
+        //     console.log(`dragging cell ${idx}`)
+        // });
 
-        gridCell.addEventListener('dragend', () => {
-            gridCell.classList.remove('dragging-cell');
+        // gridCell.addEventListener('dragend', () => {
+        //     gridCell.classList.remove('dragging-cell');
 
-            console.log('drag end')
-        });
+        //     console.log('drag end')
+        // });
 
         gridCell.addEventListener('dragover', e => {
             e.preventDefault();
 
             const draggable = document.querySelector('.dragging-random-cell');
-
-            // console.log(draggable)
-
             gridCell.appendChild(draggable)
-            // const draggableClone = draggable.cloneNode('deep')
-            // if (gridCell.children.length === 0) {
-            //     gridCell.appendChild(draggableClone);
-            // }
         });
     })
 })
