458309ae5ff4e183be4f6dcbedbe1fd5e4283ffa
Jonathan <jonathan.diaz714@gmail.com>
2023-01-25 23:52:37 -0500

    testing the drag and drop feature

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // lets place all of the code here.
     
-    console.log('testing');
+    
+    const gridCells = document.querySelectorAll('.grid-cell');
+    const randomTile = document.querySelector('.random-tile-outer-container');
+
+    randomTile.addEventListener('dragstart', () => {
+        randomTile.classList.add('dragging-random-cell');
+    });
+
+    randomTile.addEventListener('dragend', () => {
+        randomTile.classList.remove('dragging-random-cell');
+    });
+
+    gridCells.forEach((gridCell, idx) => {
+        gridCell.addEventListener('dragstart', () => {
+            gridCell.classList.add('dragging-cell');
+
+            console.log(`dragging cell ${idx}`)
+        });
+
+        gridCell.addEventListener('dragend', () => {
+            gridCell.classList.remove('dragging-cell');
+        });
+    })
 })
