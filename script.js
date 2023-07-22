056434986c4099d8da53193fe03488c8ffbdbdd1
Jonathan <jonathan.diaz714@gmail.com>
2023-07-21 19:22:34 -0500

    mobile responsive drag and drop

--- script.js
+++ script.js
@@ -1,91 +1,137 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    
-    const gridCells = document.querySelectorAll('.grid-cell');
-    const randomTile = document.querySelector('.random-tile');
-    const tileSideBarContainer = document.querySelector('.tile-side-bar-container');
+// document.addEventListener("DOMContentLoaded", () => {
+const dropboxes = document.querySelectorAll("#dropbox");
+const body = document.body;
+// body.ontouchstart = dragStart(event);
+// body.ontouchmove = drag(event);
+// body.ontouchend = drop(event);
 
-    //here is the logic for placing the tiles into the board and regenerating into the side bar
-    const mutationObserver = new MutationObserver(entries => {
-        console.log(entries)
+body.addEventListener("touchstart", (e) => dragStart(e), { passive: false });
+body.addEventListener("touchmove", (e) => drag(e), { passive: false });
+body.addEventListener("touchend", (e) => drop(e), { passive: false });
 
-        if (entries[0].removedNodes.length) {
-            const newRandomTile = document.createElement('div');
-            newRandomTile.className = 'random-tile';
-            newRandomTile.draggable = 'true';
-            newRandomTile.innerHTML = '?';
+// console.log(body);
 
-            newRandomTile.addEventListener('dragstart', () => {
-                newRandomTile.classList.add('dragging-random-cell');
-            });
+// console.log("testing");
 
-            newRandomTile.addEventListener('dragend', () => {
-                newRandomTile.classList.remove('dragging-random-cell');
-            });
+let drg, drgT, drgL, drgB, drgR;
 
-            newRandomTile.addEventListener('touchstart', () => {
-                newRandomTile.classList.add('dragging-random-cell');
-            });
+let el, avail;
 
-            newRandomTile.addEventListener('touchend', () => {
-                newRandomTile.classList.remove('dragging-random-cell');
-            });
+function dragStart(evt) {
+  el = evt.target;
 
-            tileSideBarContainer.appendChild(newRandomTile)
+  // console.log(el);
+
+  if (el.getAttribute("draggable") === "true") {
+    avail = "available";
+  } else {
+    avail = "";
+  }
+
+  // console.log("drag started");
+}
+
+function drag(evt) {
+  // console.log("dragging");
+
+  if (avail === "available") {
+    el.style.position = "absolute";
+    el.style.left = `${evt.touches[0].clientX - el.clientWidth / 2}px`;
+    el.style.top = `${evt.touches[0].clientY - el.clientHeight / 2}px`;
+    // console.log(el.style.top);
+    // console.log(el.clientHeight);
+    // console.log(evt.touches[0].clientY - el.clientHeight / 2);
+
+    drg = el.getBoundingClientRect();
+    drgT = drg.top;
+    drgB = drg.bottom;
+    drgL = drg.left;
+    drgR = drg.right;
+  } else {
+  }
+  evt.preventDefault();
+}
+
+function drop() {
+  // update on drop
+
+  for (let dropbox of dropboxes) {
+    let drp = dropbox.getBoundingClientRect();
+
+    let drpT = drp.top,
+      drpL = drp.left,
+      drpB = drp.bottom,
+      drpR = drp.right;
+
+    if (avail === "available") {
+      if (drpT < drgT && drpL < drgL && drpB > drgB && drpR > drgR) {
+        //I should only be able to append a child if
+        const tileCopy = el.cloneNode(true);
+        const parentNode = el.parentNode;
+        tileCopy.style.position = "";
+
+        if (dropbox.childNodes.length === 0) {
+          dropbox.appendChild(el);
         }
-    })
 
-    mutationObserver.observe(tileSideBarContainer, { childList: true });
+        if (
+          parentNode.className === "dragbox-cont" &&
+          parentNode.children.length === 0
+        ) {
+          parentNode.append(tileCopy);
+        }
 
-    randomTile.addEventListener('dragstart', () => {
-        randomTile.classList.add('dragging-random-cell');
-    });
+        el.style.position = "";
+      }
+    } else {
+    }
+  }
+}
 
-    randomTile.addEventListener('dragend', () => {
-        randomTile.classList.remove('dragging-random-cell');
-    });
+// this is where I want the code to live for the desktop drag and drop functionality
+const dragboxes = document.querySelectorAll("#dragbox");
+let currentBox;
 
-    randomTile.addEventListener('touchstart', () => {
-        randomTile.classList.add('dragging-random-cell');
-    });
+for (let dragbox of dragboxes) {
+  dragbox.addEventListener("dragstart", () => {
+    dragbox.classList.add("dragging-cell");
 
-    randomTile.addEventListener('touchend', () => {
-        randomTile.classList.remove('dragging-random-cell');
-    });
+    currentBox = dragbox;
+  });
 
-    randomTile.addEventListener('touchmove', e => {
-        // e.preventDefault();
-        let touchLocation = e.targetTouches[0];
+  dragbox.addEventListener("dragend", () => {
+    dragbox.classList.remove("dragging-cell");
+  });
+}
 
-        // console.log(touchLocation)
+for (let dropbox of dropboxes) {
+  dropbox.addEventListener("dragover", (e) => {
+    e.preventDefault();
 
-        randomTile.style.left = touchLocation.screenX + 'px';
-        randomTile.style.top = touchLocation.screenY + 'px';
-    })
+    // console.log(currentBox);
+    if (dropbox.childNodes.length === 0) {
+      const newTile = document.createElement("div");
+      newTile.id = "dragbox";
+      newTile.draggable = "true";
+      newTile.innerHTML = currentBox.innerHTML;
 
-    gridCells.forEach((gridCell, idx) => {
-        gridCell.addEventListener('touchmove', e => {
-            e.preventDefault();
+      newTile.addEventListener("dragstart", () => {
+        newTile.classList.add("dragging-cell");
 
-            console.log(`moving over me ${idx}`)
-        });
-        gridCell.addEventListener('touchend', e => {
-            e.preventDefault();
+        currentBox = newTile;
+      });
 
-            console.log(`ended touch here ${idx}`)
+      newTile.addEventListener("dragend", () => {
+        newTile.classList.remove("dragging-cell");
+      });
 
-            const draggable = document.querySelector('.dragging-random-cell');
-            if (gridCell.childNodes.length === 0) {
-                gridCell.appendChild(draggable)
-            }
-        })
-        gridCell.addEventListener('dragover', e => {
-            e.preventDefault();
+      const parentNode = currentBox.parentNode;
+      if (parentNode.className === "dragbox-cont") {
+        parentNode.appendChild(newTile);
+      }
 
-            const draggable = document.querySelector('.dragging-random-cell');
-            if (gridCell.childNodes.length === 0) {
-                gridCell.appendChild(draggable)
-            }
-        });
-    });
-});
+      dropbox.appendChild(currentBox);
+    }
+  });
+}
+// });
